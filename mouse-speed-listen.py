09a918c4b52a3f42a1126bc7bbf4f0d800f08913
Michael Millis <mmillis1@gmail.com>
2021-12-16 19:11:21 -0800

    change speed

--- mouse-speed-listen.py
+++ mouse-speed-listen.py
@@ -62,51 +62,51 @@ def main():
 
 
         # M1
-        # get_devices = 'xinput list|grep -w "Keychron Keychron M1 Mouse"'
-        # result = subprocess.run(get_devices, stdout=subprocess.PIPE, shell=True)
+        get_devices = 'xinput list|grep -w "Keychron Keychron M1 Mouse"'
+        result = subprocess.run(get_devices, stdout=subprocess.PIPE, shell=True)
 
-        # string_result = result.stdout.decode("utf-8").encode("ascii", "ignore")
+        string_result = result.stdout.decode("utf-8").encode("ascii", "ignore")
 
-        # lines = string_result.splitlines()
+        lines = string_result.splitlines()
 
-        # m1_id = False
-        # need_to_fix = False
-        # for line in lines:
-        #     parts = line.decode("utf-8").split('\t')
+        m1_id = False
+        need_to_fix = False
+        for line in lines:
+            parts = line.decode("utf-8").split('\t')
 
-        #     device_name = parts[0].strip()
+            device_name = parts[0].strip()
 
-        #     if device_name == 'Keychron Keychron M1 Mouse':
-        #         m1_id = parts[1].split('=')[1]
-        #         break
+            if device_name == 'Keychron Keychron M1 Mouse':
+                m1_id = parts[1].split('=')[1]
+                break
 
-        # if m1_id:
-        #     get_props = f"xinput list-props {m1_id}"
-        #     result = subprocess.run(get_props, stdout=subprocess.PIPE, shell=True)
-        #     string_result = result.stdout.decode("utf-8").encode("ascii", "ignore")
-        #     lines = string_result.splitlines()
+        if m1_id:
+            get_props = f"xinput list-props {m1_id}"
+            result = subprocess.run(get_props, stdout=subprocess.PIPE, shell=True)
+            string_result = result.stdout.decode("utf-8").encode("ascii", "ignore")
+            lines = string_result.splitlines()
 
-        #     for line in lines:
-        #         props = line.decode("utf-8").split('\t')
+            for line in lines:
+                props = line.decode("utf-8").split('\t')
 
-        #         if len(props) > 1:
-        #             if "libinput Accel Speed (" in props[1]:
-        #                 if props[2] != "-0.300000":
-        #                     need_to_fix = True
-        #                     break
+                if len(props) > 1:
+                    if "libinput Accel Speed (" in props[1]:
+                        if props[2] != "0.250000":
+                            need_to_fix = True
+                            break
 
-        #             if "libinput Accel Profile Enabled (" in props[1]:
-        #                 if props[2] != "0, 1":
-        #                     need_to_fix = True
-        #                     break
+                    if "libinput Accel Profile Enabled (" in props[1]:
+                        if props[2] != "0, 1":
+                            need_to_fix = True
+                            break
 
-        # if need_to_fix and m1_id:
-        #     command = f'xinput set-prop {m1_id} "libinput Accel Speed" -0.300000'
-        #     subprocess.run(command, stdout=subprocess.PIPE, shell=True)
+        if need_to_fix and m1_id:
+            command = f'xinput set-prop {m1_id} "libinput Accel Speed" 0.250000'
+            subprocess.run(command, stdout=subprocess.PIPE, shell=True)
 
-        #     command = f'xinput set-prop {m1_id} "libinput Accel Profile Enabled" 0, 1'
-        #     subprocess.run(command, stdout=subprocess.PIPE, shell=True)
-        #     print('Fixed')    
+            command = f'xinput set-prop {m1_id} "libinput Accel Profile Enabled" 0, 1'
+            subprocess.run(command, stdout=subprocess.PIPE, shell=True)
+            print('Fixed')    
    
 
 if __name__ == '__main__':
